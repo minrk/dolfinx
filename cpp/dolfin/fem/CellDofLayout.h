0db0029c310ad7e266f2922bb702f2f2891a4df1
Chris Richardson <chris@bpi.cam.ac.uk>
2018-11-28 14:38:50 +0000

    Add some comments

--- cpp/dolfin/fem/CellDofLayout.h
+++ cpp/dolfin/fem/CellDofLayout.h
@@ -31,12 +31,22 @@ public:
   /// Destructor
   ~CellDofLayout() = default;
 
+  /// Get the permutation of the dofs on this cell, in order to match with a
+  /// global reference orientation for a given set of input global vertex
+  /// indices. Only remaps the dofs which are on edges and facets.
+  /// @param perm
+  ///      Permutation vector required
+  /// @param vertex_indices
+  ///      Global vertex indices of the cell
   void permutation(std::vector<int>& perm, const int64_t* vertex_indices);
 
 private:
+  // Cell type
   mesh::CellType::Type _cell_type;
+
+  // Layout of dofs by entity, ordered by [dim][entity][dof], e.g. for a
+  // tetrahedron _entity_dofs[2][1][9] is the tenth dof of facet 1.
   std::vector<std::vector<std::vector<int>>> _entity_dofs;
-  std::vector<std::vector<unsigned int>> _facet_dof_coords;
 };
 } // namespace fem
 } // namespace dolfin
