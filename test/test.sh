28cce72a7bb5a2b0bb8440a16f191015995dc486
Min RK <benjaminrk@gmail.com>
2024-10-24 15:51:18 +0200

    basic mpi test

--- test/test.sh
+++ test/test.sh
@@ -10,9 +10,8 @@ export I_MPI_DEBUG=1000
 which mpiexec
 which -a python
 mpiexec --version
-mpiexec -n 1 python -c 'from mpi4py import MPI; print(MPI.COMM_WORLD.rank)'
-mpiexec -n 2 python -c 'from mpi4py import MPI; print(MPI.COMM_WORLD.rank)'
-python -c 'from mpi4py import MPI; print(MPI.COMM_WORLD.rank)'
+mpiexec -n 1 python test/test_mpi.py
+mpiexec -n 2 python test/test_mpi.py
 mpiexec -n 1 python test/test_mesh.py
 python test/test_dolfinx.py
 mpiexec -v -np 1 python -c "from mpi4py import MPI; import dolfinx; dolfinx.mesh.create_rectangle(comm=MPI.COMM_WORLD, points=((0, 0), (2, 1)), n=(32, 16))"
